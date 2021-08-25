439a176b55921074069ad8247a1611d7f8cc414e
Eldar <maratllsabri@gmail.com>
2021-08-25 16:07:48 +0300

    Поправки по второму ДЗ

--- lesson2_2.py
+++ lesson2_2.py
@@ -8,22 +8,30 @@ import pydoc
 
 in_txt = "Введите значения списка через пробел: "
 while True:
-	usr_list = list(input(in_txt).split())
-	l = len(usr_list)
-	if l > 1:
-		break
-	else:
-		print("Нужно указать больше 1 значения в списке!!!")
+    usr_list = list(input(in_txt).split())
+    l = len(usr_list)
+    if l > 1:
+        break
+    else:
+        print("Нужно указать больше 1 значения в списке!!!")
+""" Старое решение!
 new_list = usr_list.copy()
 i = 0
 if l%2 == 1:
-	l = l - 1
+    l = l - 1
 while i < l:
-	n = i % 2
-	if n == 0:
-		new_list[i+1] = usr_list[i]
-	else:
-		new_list[i-1] = usr_list[i]
-	i+=1
+    n = i % 2
+    if n == 0:
+        new_list[i+1] = usr_list[i]
+    else:
+        new_list[i-1] = usr_list[i]
+    i+=1
 print(f"Пользовательский список  : {usr_list}")
 print(f"Список с обменом значений: {new_list}")
+"""
+
+for i, v in enumerate(usr_list):
+    n=i%2
+    if n == 1:
+        usr_list[i], usr_list[i-1] = usr_list[i-1], usr_list[i]
+print(f"Список - {usr_list}")
